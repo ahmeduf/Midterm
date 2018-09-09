9146903245ba94bdbaaf395d13268764194ab7c2
ahmeduf <ahmedu.ferdous@gmail.com>
2018-09-09 18:22:50 -0400

    Fibonacci

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -7,5 +7,13 @@ public class Fibonacci {
          */
 
 
+        int prev = 0;
+        int next = 1;
+        for (int i = 1; i < 40; i++) {
+            System.out.print(next + "\t");
+            prev = next - prev;
+            next = next + prev;
+
+        }
     }
 }
