08bafda2e10cb7142684aeaad4bab4164146a786
ahmeduf <ahmedu.ferdous@gmail.com>
2018-09-10 05:31:57 -0400

    largest number

--- src/string/problems/DetermineLargestWord.java
+++ src/string/problems/DetermineLargestWord.java
@@ -11,15 +11,27 @@ public class DetermineLargestWord {
          Should return "10 biological".
          */
         String s="Human brain is a biological learning machine";
+
         Map<Integer, String> wordNLength = findTheLargestWord(s);
         //implement
 
     }
 
+
     public static Map<Integer, String> findTheLargestWord(String wordGiven){
         Map<Integer, String> map = new HashMap<Integer, String>();
-        String st = "";
         //implement
+        String[] word=wordGiven.split(" ");
+        String lw=" ";
+        for(int i=0;i<word.length;i++){
+            for(int j=i;j<word.length;j++){
+                if(word[i].length()>word[j].length()){
+                    lw=word[i];
+                }
+            }
+        }
+        System.out.println(lw);
+        System.out.println(lw.length());
 
         return map;
     }
