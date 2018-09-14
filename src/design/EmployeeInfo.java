175ddc36155c8ea1a89fcbe0071aa35f93e9e913
ahmeduf <ahmedu.ferdous@gmail.com>
2018-09-14 18:26:55 -0400

    design update

--- src/design/EmployeeInfo.java
+++ src/design/EmployeeInfo.java
@@ -61,6 +61,7 @@ public class EmployeeInfo extends AbstractEmployeeClass{
 	}
 
 	public double getSalary() {
+
 		return salary;
 	}
 
@@ -77,17 +78,44 @@ public class EmployeeInfo extends AbstractEmployeeClass{
 	}
 
 
-
 	/*
 	 * You must implement the logic for below 2 methods and
 	 * following 2 methods are prototype as well for other methods need to be design,
 	 * as you will come up with the new ideas.
 	 */
+	@Override
+	public void service(){
+		companyName="PNT.EDU";
+		System.out.println(CompanyName+"provide training for selenium,qtp & database from"+address);
+	}
+	public String assignDepartment(String departmentName){
+		this.departmentName=departmentName;
+		return departmentName;
+	}
+	public double calculateSalary(double salary){
+return salary;
+	}
+
 
 	/*
 	 * you must have multiple constructor.
 	 * Must implement below constructor.
 	 */
+
+	//employee constructor
+
+	public EmployeeInfo(){}
+
+	public EmployeeInfo(int employeeId){
+		this.employeeId=employeeId;
+	}
+	public EmployeeInfo(String empName,int employeeId,double salary){
+		this.empName=empName;
+		this.employeeId=employeeId;
+		this.salary=salary;
+	}
+
+
 
 	/*
 	 * This methods should calculate Employee bonus based on salary and performance.
@@ -101,6 +129,24 @@ public class EmployeeInfo extends AbstractEmployeeClass{
 		int total=0;
 		return total;
 	}
+	public static double calculateEmployeeBonus(double Salary, double performance){
+		performance=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Please enter the year for employee :");
+		int year=sc.nextInt();
+		if (year>=5){
+			performance=.3;
+			System.out.println("great ! good work");
+		}
+		else if (year<=2 && year >=1){
+			performance=.2;
+			System.out.println("good ! keep doing performance");
+		}
+		else System.out.println("yor are almost in your goal");
+		double total=(Salary*performance);
+		return total;
+
+	}
 
 	/*
 	 * This methods should calculate Employee Pension based on salary and numbers of years with the company.
@@ -108,10 +154,11 @@ public class EmployeeInfo extends AbstractEmployeeClass{
 	 * Hints: pension will be 5% of the salary for 1 year, 10% for 2 years with the company and so on.
 	 *
 	 */
-	public static int calculateEmployeePension(){
-		int total=0;
+
+	public static double calculateEmployeePension(double salary){
+		double total=0;
 		Scanner sc  = new Scanner(System.in);
-		System.out.println("Please enter start date in format (example: May,2015): ");
+		System.out.println("Please enter start date in format (example: May,2005): ");
 		String joiningDate = sc.nextLine();
 		System.out.println("Please enter today's date in format (example: August,2017): ");
 		String todaysDate = sc.nextLine();
@@ -120,14 +167,36 @@ public class EmployeeInfo extends AbstractEmployeeClass{
 
 		//implement numbers of year from above two dates
 		//Calculate pension
+		String startYear=convertedJoiningDate.substring(convertedJoiningDate.length()-4);
+		String currentYear=convertedJoiningDate.substring(convertedTodaysDate.length()-4);
+
+
+		int start=Integer.parseInt(startYear);
+		int current=Integer.parseInt(currentYear);
+
+		if((current-start)>= 10){
+			total= salary *.5;
+		}else if ((current-start)==9) {
+			total = salary * .4;
+		}
+			else if((current-start)==8){
+				total=salary*.3;
+			}else if ((current-start)==7){
+				total=salary*.25;
+		}else if ((current-start)==6){
+				total=salary*.2;
+
+		}else if ((current-start)==5){
+				total= salary*.1;
+		}else if ((current-start)<=4){
+				total=0;
+			System.out.println("sorry , you are not eligible");
+		}
+		else {System.out.println("you got problem");}
+		System.out.println("Employee pension is:"+"$"+total);
 
 
 		return total;
-	}
-	@Override
-	public void service(){
-		companyName="PNT.EDU";
-		System.out.println(CompanyName+"provide training for selenium,qtp & database from"+address);
 	}
 
 	@Override
@@ -147,6 +216,7 @@ public class EmployeeInfo extends AbstractEmployeeClass{
 
 	@Override
 	public void assignDepartment() {
+
 
 	}
 
@@ -171,6 +241,7 @@ public class EmployeeInfo extends AbstractEmployeeClass{
 		public static String convertDate(String date) {
 			String [] extractMonth = date.split(",");
 			String givenMonth = extractMonth[0];
+
 			int monthDate = whichMonth(givenMonth);
 			String actualDate = monthDate + "/" + extractMonth[1];
 			return actualDate;
--- src/design/FortuneEmployee.java
+++ src/design/FortuneEmployee.java
@@ -1,6 +1,6 @@
 package design;
 
-public class FortuneEmployee{
+public class FortuneEmployee {
 
 	/**
 	 * FortuneEmployee class has a main methods where you will be able to create Object from
@@ -9,12 +9,46 @@ public class FortuneEmployee{
 	 * as a Software Developer.(employee.info.system) package is given as an outline,you need to elaborate
 	 * more to design an application that will meet for fortune 500 Employee Information
 	 * Services.
-	 *
+	 * <p>
 	 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
-	 *
 	 **/
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
+		EmployeeInfo emp = new EmployeeInfo();
+		EmployeeInfo emp1 = new EmployeeInfo("farzad", 100, 4000);
+		EmployeeInfo emp2 = new EmployeeInfo("ahmed", 101, 3000);
+		EmployeeInfo emp3 = new EmployeeInfo("nasir", 102, 2000);
+		EmployeeInfo emp4 = new EmployeeInfo("fahim", 103, 1000);
+		emp.benefitLayout();
+		System.out.println("Employee 1 is : " + emp1.assignDepartment("DEVELOPER"));
+		System.out.println("Employee 2 is : " + emp2.assignDepartment("IT"));
+		System.out.println("Employee 3 is : " + emp3.assignDepartment("QA TESTER"));
+		System.out.println("Employee 4 is : " + emp4.assignDepartment("BA"));
+
+		emp1.setSalary(4000);
+		emp1.calculateSalary(emp1.getSalary());
+		emp2.setSalary(3000);
+		emp2.calculateSalary(emp2.getSalary());
+		emp3.setSalary(2000);
+		emp3.calculateSalary(emp3.getSalary());
+		emp4.setSalary(1000);
+		emp4.calculateSalary(emp4.getSalary());
+
+		emp1.calculateEmployeeBonus(emp1.getSalary(), emp1.getPerformance());
+		emp2.calculateEmployeeBonus(emp2.getSalary(), emp2.getPerformance());
+		emp3.calculateEmployeeBonus(emp3.getSalary(), emp3.getPerformance());
+		emp4.calculateEmployeeBonus(emp4.getSalary(), emp4.getPerformance());
+
+		//
+		// System.out.println(emp1.getSalary());
+		emp1.setSalary(50000);
+		emp1.calculateEmployeePension(emp1.getSalary());
+		emp2.setSalary(60000);
+		emp2.calculateEmployeePension(emp2.getSalary());
+		emp3.setSalary(70000);
+		emp3.calculateEmployeePension(emp3.getSalary());
+		emp4.setSalary(80000);
+		emp4.calculateEmployeePension(emp4.getSalary());
 
 
 	}
